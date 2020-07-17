dbe9f766f4ed4a2ff05e63955ca180b5e8379a90
Dominik Stańczak <stanczakdominik@gmail.com>
2020-07-17 18:18:25 +0200

    Post on move to aiur.github.io

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "r/AllThingsProtoss community"  # (translatable)
 BLOG_TITLE = "AllThingsProtoss"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://allthingsprotoss.github.io/"
+SITE_URL = "https://aiur.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://allthingsprotoss.github.io/"
